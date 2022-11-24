3de97635eaad0ff1c96abb43bb02e5cd60d0f118
reAlpha39 <yunusw718@gmail.com>
2022-11-24 12:48:17 +0700

    change top bar color

--- lib/widgets/main_webview.dart
+++ lib/widgets/main_webview.dart
@@ -70,84 +70,88 @@ class _MainWebViewState extends State<MainWebView> {
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () async => _customGoBack(),
-      child: SafeArea(
-          child: Column(
-        children: [
-          isLoading
-              ? LinearProgressIndicator(
-                  value: progress,
-                  backgroundColor: const Color(0xFFF1C40F),
-                  color: const Color(0xFF34495E),
-                  minHeight: 5,
-                )
-              : const SizedBox(),
-          Expanded(
-            child: InAppWebView(
-              initialUrlRequest: URLRequest(
-                url: Uri.parse('https://dewikreatif.com/'),
-              ),
-              pullToRefreshController: _pullToRefreshController,
-              onLoadStart: (_, __) {
-                if (firstLoad) {
-                  showGeneralDialog(
-                      barrierDismissible: false,
-                      context: context,
-                      pageBuilder: (_, __, ___) => const SplashScreenLoading());
-                }
-              },
-              onLoadStop: (_, __) {
-                if (firstLoad) {
-                  firstLoad = false;
-                  Navigator.pop(context);
-                }
-                _pullToRefreshController.endRefreshing();
-                setState(() {
-                  isLoading = false;
-                });
-              },
-              onLoadError: (_, __, ___, ____) =>
-                  _pullToRefreshController.endRefreshing(),
-              onProgressChanged: (controller, progress) {
-                if (progress == 100) {
+      child: Scaffold(
+        body: SafeArea(
+            child: Column(
+          children: [
+            isLoading
+                ? LinearProgressIndicator(
+                    value: progress,
+                    backgroundColor: const Color(0xFFF1C40F),
+                    color: const Color(0xFF34495E),
+                    minHeight: 5,
+                  )
+                : const SizedBox(),
+            Expanded(
+              child: InAppWebView(
+                initialUrlRequest: URLRequest(
+                  url: Uri.parse('https://dewikreatif.com/'),
+                ),
+                pullToRefreshController: _pullToRefreshController,
+                onLoadStart: (_, __) {
+                  if (firstLoad) {
+                    showGeneralDialog(
+                        barrierDismissible: false,
+                        context: context,
+                        pageBuilder: (_, __, ___) =>
+                            const SplashScreenLoading());
+                  }
+                },
+                onLoadStop: (_, __) {
+                  if (firstLoad) {
+                    firstLoad = false;
+                    Navigator.pop(context);
+                  }
                   _pullToRefreshController.endRefreshing();
-                }
-                setState(() {
-                  this.progress = progress / 100;
-                  isLoading = true;
-                });
-              },
-              onWebViewCreated: (controller) => _webViewController = controller,
-              initialOptions: _options,
-              shouldOverrideUrlLoading: (controller, navigationAction) async {
-                Uri url = navigationAction.request.url!;
-                if (![
-                  'http',
-                  'https',
-                  'file',
-                  'chrome',
-                  'data',
-                  'javascript',
-                  'about'
-                ].contains(url.scheme)) {
-                  await launchUrl(url);
-                  return NavigationActionPolicy.CANCEL;
-                } else {
                   setState(() {
+                    isLoading = false;
+                  });
+                },
+                onLoadError: (_, __, ___, ____) =>
+                    _pullToRefreshController.endRefreshing(),
+                onProgressChanged: (controller, progress) {
+                  if (progress == 100) {
+                    _pullToRefreshController.endRefreshing();
+                  }
+                  setState(() {
+                    this.progress = progress / 100;
                     isLoading = true;
                   });
-                  return NavigationActionPolicy.ALLOW;
-                }
-              },
-              androidOnPermissionRequest:
-                  (controller, origin, resources) async {
-                return PermissionRequestResponse(
-                    resources: resources,
-                    action: PermissionRequestResponseAction.GRANT);
-              },
+                },
+                onWebViewCreated: (controller) =>
+                    _webViewController = controller,
+                initialOptions: _options,
+                shouldOverrideUrlLoading: (controller, navigationAction) async {
+                  Uri url = navigationAction.request.url!;
+                  if (![
+                    'http',
+                    'https',
+                    'file',
+                    'chrome',
+                    'data',
+                    'javascript',
+                    'about'
+                  ].contains(url.scheme)) {
+                    await launchUrl(url);
+                    return NavigationActionPolicy.CANCEL;
+                  } else {
+                    setState(() {
+                      isLoading = true;
+                    });
+                    return NavigationActionPolicy.ALLOW;
+                  }
+                },
+                androidOnPermissionRequest:
+                    (controller, origin, resources) async {
+                  return PermissionRequestResponse(
+                      resources: resources,
+                      action: PermissionRequestResponseAction.GRANT);
+                },
+              ),
             ),
-          ),
-        ],
-      )),
+          ],
+        )),
+      ),
     );
   }
 }
